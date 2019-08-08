202c1714ad121a58936d22d7750983c55069c4a0
mihai.borcan <borcan.mihai@yahoo.com>
2019-08-08 18:29:01 +0300

    moved some directories

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
               int x =2;
               int y=2;
               int z = (((x++) + (++y)) >5 && ((++x)+y)<10)?1:0;
-              System.out.println(x+""+y+"" + z);
+              System.out.println(x+""+y);
 
           }
     }
